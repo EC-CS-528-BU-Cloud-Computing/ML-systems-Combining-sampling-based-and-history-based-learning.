67a1c066156e244bc19cc1b1782e8b65d107ba5e
Joshua Shterenberg <joshuashterenberg@gmailcom>
2022-11-06 20:57:45 -0500

    Update exec_test.sh

--- exec_test.sh
+++ exec_test.sh
@@ -1,4 +1,4 @@
-
+## through this entire file I assume you have a python2.7 version of pypy installed in the above directory
 ## requirements / libraries
 ../pypy2.7-v7.3.9-linux64/bin/pypy -m ensurepip
 ../pypy2.7-v7.3.9-linux64/bin/pypy -mpip install -U pip wheel
@@ -18,6 +18,7 @@
 # seed 12345
 
 ## RUN DSS
+../pypy2.7-v7.3.9-linux64/bin/pypy dss.py -r ./1000job.trace ./topo_100 128000 32 1000 1000 1000 1 regular
 ../pypy2.7-v7.3.9-linux64/bin/pypy dss.py 1000job.trace topo_100 128000 1 100 100 1000 0 patience --oracle 0 --threesigma-features-file test.3SigmaFeatures --threesigma-warmup-file test.3SigmaWarmUp --all-or-none 0 --eviction-policy 0 --split-cluster 1 --thin-node-percentage 0 --sampling-node-percentage 0 --job-arrival-time-stretch-multiplier 27.42 --thin-limit 3 --sampling-percentage 3 --common-queue 1 --auto-tunable-partition 0
 # 100 node cluster 128 GB ram 32 cores
 # 1000-ms heartbeat interval
